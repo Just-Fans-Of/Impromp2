2b5f19052395fd6364bd488b85e37f15b9a9bda9
NumbuhFour <fad4470@rit.edu>
2017-01-03 17:28:12 -0500

    Add temp channel init with game name

--- cmds/create.js
+++ cmds/create.js
@@ -15,16 +15,17 @@ class Create{
 
     // Message input, user id <snowflake>, guild id <snowflake>, channel id <snowflake>
     onCommandEntered(message, username, uid, gid, cid) {
-        console.log("Got command, sending");
-        this.bot.sendMessage({
-            to: cid,
-            message: 'Hello ' + username + ' [' + message + ']', 
-        }, (err, res) => {
-            console.log('CB', err, res);
-        });
 
         var split = message.split(' ');
         var givenName = 'Temporary Channel';
+
+        // Check if user is in game, then name it that
+        var user = this.bot.users[uid];
+        if (user.game !== null) {
+            givenName = user.game.name;
+        }
+
+        // Check if name is defined, use that
         if (split.length > 1) split.slice(0, split.length).join(' ');
 
         var channelName = this.config.tempChannelNamePrefix + username + ': ' + givenName;
