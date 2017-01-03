60a4b9c05dae0014a6025cb2f6c62bafe30b8224
Frankie DiPietro <fad4470@rit.edu>
2017-01-03 03:12:55 -0500

    Add basic game tracking

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ bot.active = () => {
     return bot.connected && bot.presenceStatus == "online";
 }
 
+var listOfUsersByGames = {};
 
 bot.on('ready', (evt) => {
     console.log('Logged in as %s - %s', bot.username, bot.id);
     checkTemporaryChannels();
-    bot.tempCheckInterval = setInterval(checkTemporaryChannels, config.tempCheckInterval);
+    bot.tempCheckInterval = setInterval(checkTemporaryChannels, config.tempChannelCheckInterval);
 });
 
 bot.on('disconnect', () => {
@@ -37,9 +38,41 @@ bot.on('any', (evt) => {
 });
 
 bot.on('presence', (user, uid, status, game, evt) => {
-    
+    // First check if user is allowed to cause temp game channels
+    var server = bot.servers[evt.d.guild_id];
+    if (inRoles(server, evt.d.roles, config.autoCreateByGameRoles)) {
+        console.log("In role");
+        var game = evt.d.game;
+        // Add user to game list if playing game
+        if (game != null) {
+           if(!listOfUsersByGames[game.name]) listOfUsersByGames[game.name] = [];
+           listOfUsersByGames[game.name].push(uid);
+           console.log("Adding user. ", listOfUsersByGames);
+           checkCommonGames();
+        }
+
+        // User not playing game, remove
+        else if (game == null) {
+            console.log("no game");
+            Object.keys(listOfUsersByGames).forEach(gameName => {
+                var userList = listOfUsersByGames[gameName];
+                console.log('userlistInd', userList.indexOf(uid));
+                if (userList.indexOf(uid) >= 0) {
+                    userList.splice(userList.indexOf(uid), 1);
+                   console.log("Removing user. ", listOfUsersByGames);
+                }
+            });
+        }
+
+    }
+
+
 });
 
+
+function checkCommonGames() {
+
+}
 
 function checkTemporaryChannels() {
     if (bot && bot.active()) {
@@ -47,13 +80,24 @@ function checkTemporaryChannels() {
         Object.keys(bot.channels).map(chanID => {
             var channel = bot.channels[chanID];
             var server = bot.servers[channel.guild_id];
-            console.log(server.name, channel.name, channel.type);
+            //console.log(server.name, channel.name, channel.type);
         });
     }
 };
 
 
-
+// Checks if any of the roles in roleIDs is in the list of roles
+// Second param is comma-delimted string
+function inRoles(server, roleIDs, roles) {
+    roles = roles.split(',').map(s => s.trim());
+    for(var i = 0; i < roleIDs.length; i++ ){
+        var roleString = server.roles[roleIDs[i]].name;
+        var filter = roles.filter(r => roleString == r);
+        console.log( "Role string", roleString, roles, filter.length);
+        if (filter.length > 0) return true;
+    }
+    return false;
+}
 
 
 // Cleanup
